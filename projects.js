10eb50a09e4b3a6238408e42f776bb5b2a99e920
NayrMu <rkmuncy2013@gmail.com>
2023-08-25 21:32:00 -0500

    Improved Carousel
    The selected number or 'project' is highlighed on the bottom carousel.

--- projects.js
+++ projects.js
@@ -1,7 +1,35 @@
-const carousel = document.QuerySelector(".carousel");
-const buttons = carousel.querySelectorAll("button");
+console.log("script is running");
 
+document.addEventListener("DOMContentLoaded", function() {
+	const carouselContainer = document.querySelector(".carousel-container");
+	const carousel = carouselContainer.querySelector(".carousel");
+	const buttons = carousel.querySelectorAll(".carousel button");
+	console.log('script is running'); 
+	buttons.forEach((button) => {
+		console.log("1");
+	});
 
-let currentIndex = 0;
+	let currentIndex = 0;
 
+	function changeNum(index) {
+		buttons.forEach((button, i) => {
+			if (i === index) {
+				button.classList.add("current");
+			}
+			else {
+				button.classList.remove("current");
+		 }
+		});
+	}
+
+	buttons.forEach((button, i) => {
+		console.log("made it");
+		button.addEventListener("click", () => {
+			console.log(currentIndex);
+			currentIndex = i;
+			console.log(currentIndex);
+			changeNum(currentIndex);
+		});
+	});
+});
 
